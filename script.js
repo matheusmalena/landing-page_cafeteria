157c34915d6ed47f7f77cd0443252c1a276b067c
Matheus Malena <matheusmalena28@gmail.com>
2024-03-06 17:54:42 -0300

    update

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ inputCheck.addEventListener('click', () => {
     const modo = inputCheck.checked ? 'dark' : 'light'
     elemento.setAttribute("data-bs-theme", modo)
 })
+
+// document.getElementById("contato").addEventListener("submit", function (event) {
+//     // Impede o envio do formulário para que possamos controlar o redirecionamento manualmente
+//     event.preventDefault();
+
+//     // Substitua esta URL pela URL da sua página normal
+//     var paginaNormal = "http://127.0.0.1:5500/index.html";
+
+//     // Redireciona para a página normal após 3 segundos (3000 milissegundos)
+//     setTimeout(function () {
+//         window.location.href = paginaNormal;
+//     }, 3000); // Redireciona após 3 segundos
+// });
